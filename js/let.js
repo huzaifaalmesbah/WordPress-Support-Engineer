dc5aae7b59d72300745f60829d8f77d293842d7a
Huzaifa Al Mesbah <huzaifah4m@gmail.com>
2023-05-06 20:21:14 +0600

    with wife name

--- js/let.js
+++ js/let.js
@@ -2,3 +2,8 @@ let name = "Hzaifa";
 const age = 24;
 console.log("My Name Is " + name);
 console.log("My Age: " + age);
+
+let wife = "Sanjida";
+let husbend = "Huzaifa";
+
+console.log("Our Name Is: " + husbend + " + " + wife);
